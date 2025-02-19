fd6e287af7b8893071e88310a052f4cd92d0d338
Rene van Spronsen <renespronsen@gmail.com>
2025-02-19 20:27:21 +0100

    health & signing

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,15 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Get, Body } from '@nestjs/common';
 import { AppService } from './app.service';
 import { MessageDto } from './dto/notification.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get('health')
+  health() {
+    return 'Server is running';
+  }
 
   @Post('send-multiple-notifications')
   async sendMultipleNotifications(@Body() body: MessageDto) {
